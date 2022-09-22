d69b9639a3f5a2b9111851aa13de9919d29dbe3c
jibon969 <jayed.swe@gmail.com>
2022-09-22 23:31:18 +0600

    added bog api

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Blog, Category
+from .serializers import CategorySerializer, BlogSerializer
 
-# Create your views here.
+
+class BlogList(APIView):
+    """
+    View to list all blog in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        queryset = Blog.objects.all()
+        serializer = BlogSerializer(queryset, many=True)
+        return Response(serializer.data)
