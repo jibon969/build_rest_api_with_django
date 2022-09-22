336b18b7e6079b037c6ea33b57889c224a0ed510
jibon969 <jayed.swe@gmail.com>
2022-09-23 00:26:46 +0600

    added search & pagination search

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,69 @@
+from rest_framework.pagination import LimitOffsetPagination
+
+from .models import Blog
+from .serializers import BlogSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Blog, Category
-from .serializers import CategorySerializer, BlogSerializer
+from rest_framework import status
+from django.http import Http404
 
 
-class BlogList(APIView):
+class BlogList(APIView, LimitOffsetPagination):
     """
-    View to list all blog in the system.
-
-    * Requires token authentication.
-    * Only admin users are able to access this view.
+    List all snippets, or create a new snippet.
     """
 
     def get(self, request, format=None):
-        """
-        Return a list of all users.
-        """
         queryset = Blog.objects.all()
-        serializer = BlogSerializer(queryset, many=True)
+        results = self.paginate_queryset(queryset, request, view=self)
+        serializer = BlogSerializer(results, many=True)
+        return self.get_paginated_response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlogDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+
+    def get_object(self, pk):
+        try:
+            return Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = BlogSerializer(snippet)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = BlogSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BlogSearch(APIView):
+    serializer_class = BlogSerializer
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        title = self.request.title
+        return Blog.objects.filter(title__icontains=title)
