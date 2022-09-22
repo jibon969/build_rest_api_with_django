d3add6cb1a073dbbdb39936a9e15a8002b396a7b
jibon969 <jayed.swe@gmail.com>
2022-09-22 23:11:09 +0600

    added blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=120)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+
