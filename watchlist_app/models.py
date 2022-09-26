1815af062e436225e04aadcacf96a482be34094b
jibon969 <jayed.swe@gmail.com>
2022-09-26 22:35:48 +0600

    added movie models

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
