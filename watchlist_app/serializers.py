e74700bbc3a8b83df2b74123b85cd8407d0352ab
jibon969 <jayed.swe@gmail.com>
2022-10-03 21:31:00 +0600

    added nested serializer

--- watchlist_app/serializers.py
+++ watchlist_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Movie, Award
+from .models import Movie, Award, Track, Album
 
 
 class MovieSerializers(serializers.ModelSerializer):
@@ -13,7 +13,7 @@ class MovieSerializers(serializers.ModelSerializer):
 
 
 class AwardSerializers(serializers.ModelSerializer):
-    name = MovieSerializers()
+    award = MovieSerializers()
 
     class Meta:
         model = Award
@@ -24,6 +24,34 @@ class AwardSerializers(serializers.ModelSerializer):
 
     def create(self, validated_data):
         award_data = validated_data.pop('award')
-        award = Award.objects.create(**validated_data)
-        movie_obj = Movie.objects.create(name=award, **award_data)
+        award_obj = Movie.objects.create(**validated_data)
+        movie_obj = Award.objects.create(award=award_obj, **award_data)
         return movie_obj
+
+    # def create(self, validated_data):
+    #     awards_data = validated_data.pop('award')
+    #     movie = Award.objects.create(**validated_data)
+    #     for award_data in awards_data:
+    #         Movie.objects.create(award=movie, **award_data)
+    #     return movie
+
+
+class TrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Track
+        fields = ['order', 'title', 'duration']
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    tracks = TrackSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Album
+        fields = ['album_name', 'artist', 'tracks']
+
+    def create(self, validated_data):
+        tracks_data = validated_data.pop('tracks')
+        album = Album.objects.create(**validated_data)
+        for track_data in tracks_data:
+            Track.objects.create(album=album, **track_data)
+        return album
