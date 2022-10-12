7cc20c47e3a80db42b68799befae769f6e19a60a
jibon969 <jayed.swe@gmail.com>
2022-10-12 23:24:23 +0600

    added create method

--- home/serializers.py
+++ home/serializers.py
@@ -12,6 +12,18 @@ class StudentSerializers(serializers.ModelSerializer):
             'roll'
         ]
 
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.dept = validated_data.get('dept', instance.dept)
+        instance.roll = validated_data.get('roll', instance.roll)
+
+        instance.save()
+        return instance
+
+
 
 
 
