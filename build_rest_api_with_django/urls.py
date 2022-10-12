81eec3bff16efcbf9f153f604a6865b06040581f
jibon969 <jayed.swe@gmail.com>
2022-10-12 23:29:47 +0600

    added nested_serializer app

--- build_rest_api_with_django/urls.py
+++ build_rest_api_with_django/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('', include('home.urls')),
+    path('', include('nested_serializer.urls')),
     path('', include('watchlist_app.urls')),
 ]
 
