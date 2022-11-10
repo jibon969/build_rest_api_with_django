4bbf51fc4bce3b5963335dd594b7d3e6c2d94451
jayed969 <jayed.swe@gmail.com>
2022-11-10 11:22:45 +0600

    added product table

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=500)
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=450)
+    app_price = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True, default=0)
+    weight = models.CharField(max_length=20, help_text='20ml/20gm', null=True, blank=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
