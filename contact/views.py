cf85fa461355f7850e82d166a70a2d16fb834dd9
jayed969 <jayed.swe@gmail.com>
2022-11-29 12:27:55 +0600

    added contact details

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ContactSerializer
 from .models import Contact
+from django.http import Http404
 from rest_framework.views import APIView
 
 
@@ -48,11 +49,64 @@ from rest_framework.views import APIView
 #         }
 #         return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
+#
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def contact_detail(request, pk):
+#     """
+#     Retrieve, update or delete a code contact.
+#     {
+#         "id": 3,
+#         "subject": "hi",
+#         "name": "SWE",
+#         "email": "jibon.belasea@gmail.com",
+#         "message": "test message"
+#     }
+#
+#     """
+#     try:
+#         queryset = Contact.objects.get(pk=pk)
+#     except Contact.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == "GET":
+#         serializer = ContactSerializer(queryset)
+#         context = {
+#             "results": serializer.data,
+#             "status": True,
+#             "message": "Your message successfully send !"
+#         }
+#         return Response(context)
+#
+#     elif request.method == "PUT":
+#         serializer = ContactSerializer(queryset, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             context = {
+#                 "results": serializer.data,
+#                 "status": True,
+#                 "message": "Update Successfully Complete !"
+#             }
+#             return Response(context)
+#         context = {
+#             "results": serializer.errors,
+#             "status": False,
+#             "message": "Cannot Successfully Complete !"
+#         }
+#         return Response(context, status=status.HTTP_400_BAD_REQUEST)
+#
+#     elif request.method == "DELETE":
+#         queryset.delete()
+#         context = {
+#             "status": True,
+#             "message": "Contact Successfully deleted !"
+#         }
+#         return Response(context, status=status.HTTP_204_NO_CONTENT)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def contact_detail(request, pk):
+
+class ContactDetail(APIView):
     """
-    Retrieve, update or delete a code contact.
+    Retrieve, update or delete a Contact instance.
+    # Example of json data
     {
         "id": 3,
         "subject": "hi",
@@ -60,24 +114,27 @@ def contact_detail(request, pk):
         "email": "jibon.belasea@gmail.com",
         "message": "test message"
     }
-    
     """
-    try:
-        queryset = Contact.objects.get(pk=pk)
-    except Contact.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == "GET":
+    def get_object(self, pk):
+        try:
+            return Contact.objects.get(pk=pk)
+        except Contact.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        queryset = self.get_object(pk)
         serializer = ContactSerializer(queryset)
         context = {
             "results": serializer.data,
             "status": True,
             "message": "Your message successfully send !"
         }
-        return Response(context)
+        return Response(context, status=status.HTTP_200_OK)
 
-    elif request.method == "PUT":
-        serializer = ContactSerializer(queryset, data=request.data)
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = ContactSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             context = {
@@ -88,12 +145,13 @@ def contact_detail(request, pk):
             return Response(context)
         context = {
             "results": serializer.errors,
-            "status": False,
+            "status": True,
             "message": "Cannot Successfully Complete !"
         }
         return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    def delete(self, request, pk, format=None):
+        queryset = self.get_object(pk)
         queryset.delete()
         context = {
             "status": True,
