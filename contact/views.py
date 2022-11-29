af5b36fec73bf0bb2255d003fa9ff1e870f9e5d8
jayed969 <jayed.swe@gmail.com>
2022-11-29 10:59:11 +0600

    remove function based view

--- contact/views.py
+++ contact/views.py
@@ -1,51 +1,3 @@
-# from rest_framework import status
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from .serializers import ContactSerializer
-# from .models import Contact
-#
-#
-# @api_view(['GET', 'POST'])
-# def contact_list(request):
-#     """
-#     List all code Contact, or create a new contact.
-#     # Example of json files
-#
-#     {
-#         "subject": "Hello",
-#         "name": "SWE",
-#         "email": "jibon.belasea@gmail.com",
-#         "message": "test"
-#     }
-#     """
-#     if request.method == 'GET':
-#         snippets = Contact.objects.all()
-#         serializer = ContactSerializer(snippets, many=True)
-#         context = {
-#             "data": serializer.data,
-#             "status": True,
-#             "msg": "Get all contact list"
-#         }
-#         return Response(context)
-#
-#     elif request.method == 'POST':
-#         serializer = ContactSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             context = {
-#                 "data": serializer.data,
-#                 "status": True,
-#                 "msg": "Your message successfully created !"
-#             }
-#             return Response(context, status=status.HTTP_201_CREATED)
-#         context = {
-#             "data": serializer.errors,
-#             "status": False,
-#             "msg": "Failed to insert new record"
-#         }
-#         return Response(context, status=status.HTTP_400_BAD_REQUEST)
-
-
 from .models import Contact
 from .serializers import ContactSerializer
 from rest_framework.views import APIView
@@ -60,7 +12,7 @@ class ContactList(APIView):
     {
         "subject": "Hello, there",
         "name": "Jibon Ahmed",
-        "email": "jibon.py@gmail.com",
+        "email": "jayed.swe@gmail.com",
         "message": "test message"
     }
     """
