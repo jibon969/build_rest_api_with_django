3bffcf878ad3f32e586a4de9d1d66c20b3255483
jayed969 <jayed.swe@gmail.com>
2022-12-14 09:53:23 +0600

    added snippets models

--- rest_app/admin.py
+++ rest_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Snippet
 
-# Register your models here.
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ['title', 'code']
+
+    class Meta:
+        model = Snippet
+
+
+admin.site.register(Snippet, SnippetAdmin)
--- rest_app/models.py
+++ rest_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Snippet(models.Model):
+    title = models.CharField(required=False, allow_blank=True, max_length=100)
+    code = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.title
