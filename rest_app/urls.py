ca8564851c08f5a9a2b786c8fa8c28c80682ae55
jayed969 <jayed.swe@gmail.com>
2022-12-15 22:17:11 +0600

    added path

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('snippet/', views.SnippetView.as_view())
+    path('snippet/', views.SnippetView.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
