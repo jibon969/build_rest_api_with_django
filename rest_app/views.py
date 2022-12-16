e665ba334e827b0e78b20f4f3f9f1fe66867aafd
jayed969 <jayed.swe@gmail.com>
2022-12-16 22:35:21 +0600

    added commit

--- rest_app/views.py
+++ rest_app/views.py
@@ -79,8 +79,18 @@ class SnippetDetail(APIView):
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            context = {
+                "status": False,
+                "message": "Update Snippet",
+                "data": serializer.data,
+            }
+            return Response(context)
+        context = {
+            "status": False,
+            "message": "Not Update",
+            "data": serializer.errors,
+        }
+        return Response(context, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
         snippet = self.get_object(pk)
