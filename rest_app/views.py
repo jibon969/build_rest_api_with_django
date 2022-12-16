cbf3ddb6120426bc1674f275f8fac643756c2df6
jayed969 <jayed.swe@gmail.com>
2022-12-17 01:32:31 +0600

    added listview

--- rest_app/views.py
+++ rest_app/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
-
 from .models import Snippet
+from rest_framework import mixins
+from rest_framework import generics
 from .serializers import SnippetSerializer
 from rest_framework.views import APIView
 from rest_framework import status
@@ -100,3 +101,14 @@ class SnippetDetail(APIView):
         }
         snippet.delete()
         return Response(context, status=status.HTTP_204_NO_CONTENT)
+
+
+class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
