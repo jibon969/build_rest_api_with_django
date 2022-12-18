31eb76829a5be0798f574b2635e5261a34899ba2
jayed969 <jayed.swe@gmail.com>
2022-12-18 19:29:59 +0600

    added function based view

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('snippet/', views.SnippetView.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippet-list/', views.SnippetList.as_view())
+    path('snippet-list/', views.snippet_list)
 ]
