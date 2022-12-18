be6b62733cfbf2032628d7c23737d52a29da8330
jayed969 <jayed.swe@gmail.com>
2022-12-18 19:35:48 +0600

    added snippets

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('snippet/', views.SnippetView.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippet-list/', views.snippet_list)
+    path('snippet-list/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail)
 ]
