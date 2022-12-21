4cbadaadd610397fa1f28489242d950b71c1e7ff
jayed969 <jayed.swe@gmail.com>
2022-12-21 22:15:30 +0600

    added serializer

--- nested_serializer/serializers.py
+++ nested_serializer/serializers.py
@@ -2,22 +2,20 @@ from rest_framework import serializers
 from .models import Profile, Hobby
 
 
-class HobbySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hobby
-        fields = '__all__'
-
-
 class ProfileSerializer(serializers.ModelSerializer):
-    user_hobby = HobbySerializer(many=True)
 
     class Meta:
         model = Profile
         fields = '__all__'
 
-    def create(self, validated_data):
-        user_hobby = validated_data.pop('user_hobby')
-        profile_instance = Profile.objects.create(**validated_data)
-        for hobby in user_hobby:
-            Hobby.objects.create(user=profile_instance, **hobby)
-        return profile_instance
+
+class HobbySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hobby
+        fields = [
+            'user',
+            'name',
+            'description',
+            'status'
+        ]
+
