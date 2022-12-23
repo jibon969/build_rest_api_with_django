bcbadf443dbe049c026e6baee6f410e3da631eea
jayed969 <jayed.swe@gmail.com>
2022-12-23 21:58:31 +0600

    register models

--- rest_app/admin.py
+++ rest_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Snippet
+from .models import Snippet, Album, Track
 
 
 class SnippetAdmin(admin.ModelAdmin):
@@ -10,3 +10,23 @@ class SnippetAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Snippet, SnippetAdmin)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['album_name', 'artist']
+
+    class Meta:
+        model = Album
+
+
+admin.site.register(Album, AlbumAdmin)
+
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ['title', 'duration']
+
+    class Meta:
+        model = Track
+
+
+admin.site.register(Track, TrackAdmin)
