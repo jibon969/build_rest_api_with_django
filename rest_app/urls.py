c497d10854049256c9b5eed0c4d71e7c0def7e7c
jayed969 <jayed.swe@gmail.com>
2022-12-24 21:24:02 +0600

    added AlbumAPIView

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('snippet/', views.SnippetView.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
     path('snippet-list/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail)
+    path('snippets/<int:pk>/', views.snippet_detail),
+    path('album/', views.AlbumAPIView.as_view())
 ]
