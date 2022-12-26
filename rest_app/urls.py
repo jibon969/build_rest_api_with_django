e482167468e11d10e25d8570921d9f9de73b076d
jayed969 <jayed.swe@gmail.com>
2022-12-26 17:08:42 +0600

    added rspace urls

--- rest_app/urls.py
+++ rest_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('snippet/', views.SnippetView.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+
     path('snippet-list/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 
